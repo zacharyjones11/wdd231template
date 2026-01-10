902d5e7322615dec5e9b5eff98caad772add0c26
zacharyjones11 <jon23074@byui.edu>
2026-01-10 11:34:56 -0700

    Made changes to make the website readable and vibrant. Also imported data from a database.

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ Use this as a starting point to complete the WDD 231 personal activity. We will 
 ## Other commands
 
 - `npm run build` to build final files when you are ready to turn in.
+
+ ## Website URL
+ 
+https://tubular-belekoy-e359ce.netlify.app/
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+// 1. Update the disclaimer link
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+// 2. Update the page title
+document.querySelector("title").innerHTML = parkData.fullName;
+
+// 3. Update the hero image
+document.querySelector(".hero-banner img").src = parkData.images[0].url;
+document.querySelector(".hero-banner img").alt = parkData.images[0].altText;
+
+// 4. Template function for park info (name, designation, states)
+function parkInfoTemplate(info) {
+  return `<a href="/" class="hero-banner__title">${info.fullName}</a>
+  <p class="hero-banner__subtitle">
+    <span>${info.designation}</span>
+    <span>${info.states}</span>
+  </p>`;
+}
+
+// Use the template to update the hero content
+document.querySelector(".hero-banner__content").innerHTML = parkInfoTemplate(parkData);
